bf7c89e053ecd878533d03d8cb19cbfa734414dc
Chris Thorne <u1400900@umail.utah.edu>
2022-01-30 21:27:08 -0700

    routes, server

---
+++ routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = (app) => {
+    app.get("/api/notes", (req, res) => {
+        let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        res.json(savedNotes);
+    });
+    app.post("/api/notes", (req, res) => {
+        let newNotes = req.body;
+        newNotes.id = uuidv4();
+        let notesData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        notesData.push(newNotes);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesData));
+        res.json(notesData);
+    });
+    app.delete("/api/notes/:id", (req, res) => {
+        let noteId = req.params.id;
+        let notesData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let newSet = notesData.filter( note => note.id !== noteId );
+        fs.writeFileSync('./db/db.json', JSON.stringify(newSet));
+        res.json(newSet);
+    });
+};
